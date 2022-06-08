6cb1da974f137d8de9074fe7f4a42357eb2416f2
nick <wnsgur9701@naver.com>
2022-06-08 12:09:45 +0900

    DP 완료

---
+++ dp_11726.py
@@ -0,0 +1,28 @@
+"""
+1. 아이디어
+(점화식을 한번에 생각해내기는 어렵다)
+- 점화식 : An = An-1 + An-2
+- N값 구하기 위해, for문 3부터 N까지의 값을 구해주기
+- 이전값, 이전이전값 더해서, 10007로 나누어서 출력
+
+2. 시간 복잡도
+- O(N)
+
+3. 자료구조
+- DP값 저장하는 배열 : int[]
+- 최대값 : 10007보다 작음
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+rs = [0,1,2]
+
+for i in range(3, n+1):
+    rs.append(rs[i-1] + rs[i-2])
+
+answer = rs[n] % 10007
+print(answer)
